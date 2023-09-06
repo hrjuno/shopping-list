cac0748a13096fc06a970fa3d57ef92aba96409d
hrjuno <harjunoabdullah@gmail.com>
2023-09-06 23:36:15 +0700

    main/ to /

--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP A'
+        'name': 'Harjuno Abdullah',
+        'class': 'PBP C'
     }
 
     return render(request, "main.html", context)
